2d9b5b738b7d5a9e77dd69ad2614c9d60ac99fb3
n1nj4sec <contact@n1nj4.eu>
2015-05-23 23:39:36 +0200

    first commit

---
+++ memorpy/__init__.py
@@ -0,0 +1,21 @@
+# Author: Nicolas VERDIER
+# This file is part of memorpy.
+#
+# memorpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# memorpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with memorpy.  If not, see <http://www.gnu.org/licenses/>.
+
+from MemWorker import *
+from Locator import *
+from Address import *
+from Process import ProcessException
+
