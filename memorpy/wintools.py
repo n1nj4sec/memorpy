06e4e7be7815c102e8058ebe0752bdb9abb2a961
n1nj4sec <contact@n1nj4.eu>
2015-05-24 16:49:22 +0200

    wintools.py added

--- memorpy/wintools.py
+++ memorpy/wintools.py
@@ -14,9 +14,21 @@
 # You should have received a copy of the GNU General Public License
 # along with memorpy.  If not, see <http://www.gnu.org/licenses/>.
 
-from MemWorker import *
-from Locator import *
-from Address import *
-from Process import *
-from wintools import *
-from utils import *
+import win32gui
+import win32con
+import win32console
+import time
+
+def start_winforeground_daemon():
+	import threading
+	t=threading.Thread(target=window_foreground_loop)
+	t.daemon=True
+	t.start()
+	
+def window_foreground_loop(timeout=20):
+	""" set the windows python console to the foreground (for example when you are working with a fullscreen program) """
+	hwnd=int(win32console.GetConsoleWindow())
+	while True:
+		win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0,0,0,0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+		time.sleep(timeout)
+	
