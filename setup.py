e561a049f9f7bd89f102f81d0ec764261a6914f6
n1nj4sec <contact@n1nj4.eu>
2016-10-14 21:43:00 +0200

    adding install script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+
+from distutils.core import setup
+
+setup(name='memorpy',
+    version='1.0',
+    description='Python library using ctypes to search/edit windows/linux programs memory',
+    author='Nicolas VERDIER',
+    author_email='contact@n1nj4.eu',
+    url='https://github.com/n1nj4sec/memorpy',
+    packages=['memorpy'],
+)
