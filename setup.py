892e54e1e2f013d6edb90cf7bb0236563e76d8d4
n1nj4sec <contact@n1nj4.eu>
2017-04-05 21:43:46 +0200

    update setup.py from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
-# -*- coding: UTF8 -*-
+import sys
+import os
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-from distutils.core import setup
+if sys.version_info < (2, 5):
+    sys.exit("requires python 2.5 and up")
+if sys.version_info[0] == 3:
+	sys.exit("not compatible python 3")
 
-setup(name='memorpy',
-    version='1.1',
-    description='Python library using ctypes to search/edit windows/linux programs memory',
-    author='Nicolas VERDIER',
-    author_email='contact@n1nj4.eu',
-    url='https://github.com/n1nj4sec/memorpy',
-    packages=['memorpy'],
+here = os.path.dirname(__file__)
+
+setup(name = "memorpy",
+    version = '1.2',
+    description = "Python library using ctypes to search/edit windows/linux programs memory",
+    author = "Nicolas VERDIER",
+    author_email = "contact@n1nj4.eu",
+    maintainer_email = "contact@n1nj4.eu",
+    license = "BSD",
+    url = "https://github.com/n1nj4sec/memorpy",
+    packages = [
+        'memorpy',
+    ],
+    install_requires = [],
+    platforms = ["POSIX", "Windows"],
+    use_2to3 = False,
+    zip_safe = False,
+    long_description = open(os.path.join(here, "README.md"), "r").read(),
 )
+
+
