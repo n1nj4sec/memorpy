9326ef1d6867e390bdf35532eac478c6d183d93f
Oleksii Shevchuk <alxchk@gmail.com>
2017-05-21 21:14:13 +0300

    Add SunOS support

--- memorpy/Process.py
+++ memorpy/Process.py
@@ -7,5 +7,7 @@ if sys.platform=='win32':
     from WinProcess import WinProcess as Process
 elif sys.platform=='darwin':
     from OSXProcess import OSXProcess as Process
+elif 'sunos' in sys.platform:
+    from SunProcess import SunProcess as Process
 else:
     from LinProcess import LinProcess as Process
